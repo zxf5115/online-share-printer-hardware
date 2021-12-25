d990eec01086dc0f55a9757e330e23ffdfcfe7e6
Zhang XiaoFei <1326336909@qq.com>
2021-12-25 11:15:19 +0800

    开发任务

--- app/Sockets/Socket.php
+++ app/Sockets/Socket.php
@@ -9,6 +9,20 @@ use Hhxsv5\LaravelS\Swoole\Socket\TcpSocket;
  */
 class Socket extends TcpSocket
 {
+  /**
+   * @author zhangxiaofei [<1326336909@qq.com>]
+   * @dateTime 2021-12-25
+   * ------------------------------------------
+   * 连接
+   * ------------------------------------------
+   *
+   * 客户端连接
+   *
+   * @param Server $server [description]
+   * @param [type] $fd [description]
+   * @param [type] $reactorId [description]
+   * @return [type]
+   */
   public function onConnect(Server $server, $fd, $reactorId)
   {
     \Log::info('New TCP connection', [$fd]);
@@ -16,13 +30,29 @@ class Socket extends TcpSocket
     $server->send($fd, 'Welcome to Socket.');
   }
 
+
+  /**
+   * @author zhangxiaofei [<1326336909@qq.com>]
+   * @dateTime 2021-12-25
+   * ------------------------------------------
+   * 接收消息
+   * ------------------------------------------
+   *
+   * 接收客户端消息
+   *
+   * @param Server $server [description]
+   * @param [type] $fd [description]
+   * @param [type] $reactorId [description]
+   * @param [type] $data [description]
+   * @return [type]
+   */
   public function onReceive(Server $server, $fd, $reactorId, $data)
   {
     \Log::info('Received data', [$fd, $data]);
 
     $server->send($fd, 'LaravelS: ' . $data);
 
-    if ($data === "quit\r\n")
+    if ($data === "quit0xff0x**")
     {
       $server->send($fd, 'LaravelS: bye' . PHP_EOL);
       $server->close($fd);
