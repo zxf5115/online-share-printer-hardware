e034b21e63f13c0c3bfd6a70074941e599b4aa8d
Zhang XiaoFei <1326336909@qq.com>
2021-12-25 14:12:09 +0800

    开发任务

--- app/Sockets/Socket.php
+++ app/Sockets/Socket.php
@@ -70,7 +70,7 @@ class Socket extends TcpSocket
 \Log::error($client_time);
       $timestamp = bcsub(time(), $client_time);
 
-      $message = 'heart beat delay:' . $timestamp . '\r';
+      $message = 'heart beat delay:' . $timestamp . PHP_EOL;
 \Log::error($message);
       $server->send($client_id, $message);
     }
