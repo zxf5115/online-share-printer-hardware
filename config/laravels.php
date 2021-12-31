3e1653d59c3c651eed5c4b6a9706844d327bfbac
Zhang XiaoFei <1326336909@qq.com>
2021-12-31 19:17:28 +0800

    修复问题

--- config/laravels.php
+++ config/laravels.php
@@ -200,12 +200,11 @@ return [
     */
 
     'timer' => [
-        'enable'          => env('LARAVELS_TIMER', false),
+        'enable'          => true,
 
         // The list of cron job
         'jobs'            => [
-            // Enable LaravelScheduleJob to run `php artisan schedule:run` every 1 minute, replace Linux Crontab
-            // Hhxsv5\LaravelS\Illuminate\LaravelScheduleJob::class,
+            \App\Crontab\Socket\PrintCrontab::class;
         ],
 
         // Max waiting time of reloading
@@ -296,7 +295,7 @@ return [
         'dispatch_mode'      => env('LARAVELS_DISPATCH_MODE', 2),
         'reactor_num'        => env('LARAVELS_REACTOR_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() : 8),
         'worker_num'         => env('LARAVELS_WORKER_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 4 : 8),
-        //'task_worker_num'    => env('LARAVELS_TASK_WORKER_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 4 : 8),
+        'task_worker_num'    => env('LARAVELS_TASK_WORKER_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 4 : 8),
         'task_ipc_mode'      => 1,
         'task_max_request'   => env('LARAVELS_TASK_MAX_REQUEST', 100000),
         'task_tmpdir'        => @is_writable('/dev/shm/') ? '/dev/shm' : '/tmp',
